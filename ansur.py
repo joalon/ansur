c1e6af676ce49910698950b0e1d17bf327d30d75
joalon <joakimlonnegren@gmail.com>
2019-03-27 08:27:32 -0300

    Implements checking if package exists and fetching

--- ansur.py
+++ ansur.py
@@ -1,10 +1,75 @@
-#!/bin/python
+from ansible.module_utils.basic import AnsibleModule
+from urllib import request
+from urllib.error import HTTPError
+import os
 
-import datetime
-import json
+AUR_BASE_URL = "https://aur.archlinux.org/"
+TMP_DIR = ""
+USER = ""
 
-date = str(datetime.datetime.now())
-print(json.dumps({
-    "time" : date
-}))
+PACMAN_CMD = None
+GIT_CMD = None
+MAKEPKG_CMD = None
 
+def aur_package_exists(module, name):
+    try:
+        aur_url = AUR_BASE_URL + "packages/{}".format(name)
+        request.urlopen(aur_url).read()
+        return True
+    except HTTPError:
+        return False
+
+
+def aur_package_fetch(module, name):
+    package_path = os.path.join(TMP_DIR, name)
+    cmd = ["git"]
+    if os.path.exists(package_path):
+        os.chdir(package_path)
+        cmd.append("pull")
+        if not module.check_mode:
+            module.run_command(cmd, check_rc=True, cwd=package_path)
+    else:
+        aur_repo_url = AUR_BASE_URL + "{}.git".format(name)
+        cmd.append(["clone", aur_repo_url])
+        if not module.check_mode:
+            module.run_command(cmd, check_rc=True, cwd=package_path)
+    return True
+
+
+def aur_package_install(module, name):
+    return 0
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            state   = dict(default='present', choices=['present', 'absent', 'latest']),
+            name    = dict(required=True)
+        ),
+        supports_check_mode = False
+    )
+
+    name = module.params['name']
+    state = module.params['state']
+
+
+    global PACMAN_CMD
+    PACMAN_CMD = module.get_bin_path('pacman', True)
+
+    global GIT_CMD
+    GIT_CMD = module.get_bin_path('git', True)
+
+    global MAKEPKG_CMD
+    MAKEPKG_CMD = module.get_bin_path('makepkg', True)
+
+    if aur_package_exists(module, name):
+        if state == 'present':
+            aur_package_fetch(module, name)
+            aur_package_install(module, name)
+            module.exit_json(changed=True, something_else=12345)
+        
+    module.fail_json(msg="something happened!")
+        
+
+if __name__ == '__main__':
+    main()
