9e7f54050c233c923df5425748df02058bd308bb
joalon <joakimlonnegren@gmail.com>
2019-07-24 21:58:14 -0300

    Implements the package_exists and package_fetch function. Will have to rethink them in the future, they don't take arch or repository into account

--- ansur.py
+++ ansur.py
@@ -1,11 +1,12 @@
 from ansible.module_utils.basic import AnsibleModule
 from urllib import request
 from urllib.error import HTTPError
+from pathlib import Path
+
 import os
 
 AUR_BASE_URL = "https://aur.archlinux.org/"
-TMP_DIR = ""
-USER = ""
+TMP_DIR = Path('/tmp/')
 
 PACMAN_CMD = None
 GIT_CMD = None
@@ -18,47 +19,58 @@ def aur_package_exists(module, name):
         return True
     except HTTPError:
         return False
-
+    except request.exceptions.Timeout as e:
+        module.fail_json(changed=False, msg=str(e))
+    except request.exceptions.TooManyRedirects:
+        module.fail_json(changed=False, msg=str(e))
 
 def aur_package_fetch(module, name):
-    package_path = os.path.join(TMP_DIR, name)
+    package_path = TMP_DIR / name
     cmd = [GIT_CMD]
-    if os.path.exists(package_path):
-        os.chdir(package_path)
+    if package_path.is_dir():
         cmd.append("pull")
         if not module.check_mode:
-            module.run_command(cmd, check_rc=True, cwd=package_path)
+            output = module.run_command(' '.join(str(s) for s in cmd), cwd=str(package_path.absolute()))
+            if not output[0]:
+                module.fail_json(msg="Already up to date")
+            module.fail_json(msg=output)
     else:
         aur_repo_url = AUR_BASE_URL + "{}.git".format(name)
-        cmd.append(["clone", aur_repo_url])
+        cmd.append("clone")
+        cmd.append(aur_repo_url)
+        cmd.append(str(package_path.absolute()))
         if not module.check_mode:
-            module.run_command(cmd, check_rc=True, cwd=package_path)
+            output = module.run_command(' '.join(str(s) for s in cmd), cwd=str(package_path.absolute()))
+            if not output[0]:
+                module.fail_json(msg="Success!")
+            module.fail_json(msg=output)
     return True
 
-
 def aur_package_install(module, name):
-    package_path = os.path.join(TMP_DIR, name)
-    module.run_command(["makepkg", "--syncdeps"], check_rc=True, cwd=package_path)
-    module.run_command(["makepkg", "--install"], check_rc=True, cwd=package_path)
-    module.run_command(["makepkg", "--clean"], check_rc=True, cwd=package_path)
-    return 0
+    package_path = TMP_DIR / name
 
+    if not module.check_mode:
+        module.run_command(["makepkg", "--syncdeps"], cwd=str(package_path.absolute()))
+        module.run_command(["makepkg", "--install"], cwd=str(package_path.absolute()))
+        module.run_command(["makepkg", "--clean"], cwd=str(package_path.absolute()))
+    return True
 
 def aur_package_remove(module, name):
+    output = module.run_command(["pacman", "-Rs", name])
+    module.fail_json(msg=output)
     return 0
 
 def main():
     module = AnsibleModule(
         argument_spec = dict(
-            state   = dict(required=True, default='present', choices=['present', 'absent', 'latest']),
+            state   = dict(required=True, choices=['present', 'absent', 'latest']),
             name    = dict(required=True)
         ),
-        supports_check_mode = False
+        supports_check_mode = True
     )
 
     name = module.params['name']
     state = module.params['state']
-
 
     global PACMAN_CMD
     PACMAN_CMD = module.get_bin_path('pacman', True)
@@ -69,14 +81,16 @@ def main():
     global MAKEPKG_CMD
     MAKEPKG_CMD = module.get_bin_path('makepkg', True)
 
-    if aur_package_exists(module, name):
-        if state == 'present' or state == 'latest':
-            aur_package_fetch(module, name)
-            aur_package_install(module, name)
-            module.exit_json(changed=True, something_else=12345)
-        
-    module.fail_json(msg="something happened!")
-        
+    if not aur_package_exists(module, name):
+        module.fail_json(changed=False, msg='Couldn\'t find package ' + name)
+
+    if state == 'present' or state == 'latest':
+        aur_package_fetch(module, name)
+        installed = aur_package_install(module, name)
+        module.exit_json(changed=True)
+    elif state == 'absent':
+        aur_package_remove(name)
+        module.exit_json(changed=True)
 
 if __name__ == '__main__':
     main()
