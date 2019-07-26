ddba58da318c618db086e4b321bd40597b51636d
joalon <joakimlonnegren@gmail.com>
2019-07-26 05:58:05 -0300

    Uses the more dependable aur api to find packages instead of scraping the web page.

--- ansur.py
+++ ansur.py
@@ -1,52 +1,38 @@
 from ansible.module_utils.basic import AnsibleModule
 from urllib import request
+from urllib.parse import urlencode, quote_plus
 from urllib.error import HTTPError
+from json import loads
 from pathlib import Path
 
 import os
 
-AUR_BASE_URL = "https://aur.archlinux.org/"
-TMP_DIR = Path('/tmp/')
+TMP_DIR = Path('/tmp')
 
 PACMAN_CMD = None
 GIT_CMD = None
 MAKEPKG_CMD = None
 
-def aur_package_exists(module, name):
-    try:
-        aur_url = AUR_BASE_URL + "packages/{}".format(name)
-        request.urlopen(aur_url).read()
-        return True
-    except HTTPError:
-        return False
-    except request.exceptions.Timeout as e:
-        module.fail_json(changed=False, msg=str(e))
-    except request.exceptions.TooManyRedirects:
-        module.fail_json(changed=False, msg=str(e))
+def find_aur_package(module, name, url='https://aur.archlinux.org/rpc.php'):
+    search_term = urlencode({'type': 'search', 'arg': name})
+    result = request.urlopen("%s?%s" % (url, search_term)).read()
+    parsed_result= loads(result)
 
-def aur_package_fetch(module, name):
-    package_path = TMP_DIR / name
-    cmd = [GIT_CMD]
-    if package_path.is_dir():
-        cmd.append("pull")
-        if not module.check_mode:
-            output = module.run_command(' '.join(str(s) for s in cmd), cwd=str(package_path.absolute()))
-            if not output[0]:
-                module.fail_json(msg="Already up to date")
-            module.fail_json(msg=output)
-    else:
-        aur_repo_url = AUR_BASE_URL + "{}.git".format(name)
-        cmd.append("clone")
-        cmd.append(aur_repo_url)
-        cmd.append(str(package_path.absolute()))
-        if not module.check_mode:
-            output = module.run_command(' '.join(str(s) for s in cmd), cwd=str(package_path.absolute()))
-            if not output[0]:
-                module.fail_json(msg="Success!")
-            module.fail_json(msg=output)
-    return True
+    if parsed_result['resultcount'] > 0:
+        return parsed_result
+    return {}
 
-def aur_package_install(module, name):
+def fetch_aur_package(module, package_dict, repo_url='https://aur.archlinux.org'):
+    download_url = repo_url + package_dict['url_path']
+
+    download_path = TMP_PATH / package_dict['name']
+
+    if download_path.is_dir():
+        module.run_command([GIT_CMD, 'pull'], cwd=download_path)
+    module.run_command([GIT_CMD, 'clone', download_url], cwd=TMP_DIR)
+
+
+def install_aur_package(module, name):
     package_path = TMP_DIR / name
 
     if not module.check_mode:
@@ -55,7 +41,7 @@ def aur_package_install(module, name):
         module.run_command(["makepkg", "--clean"], cwd=str(package_path.absolute()))
     return True
 
-def aur_package_remove(module, name):
+def remove_aur_package(module, name):
     output = module.run_command(["pacman", "-Rs", name])
     module.fail_json(msg=output)
     return 0
@@ -81,15 +67,18 @@ def main():
     global MAKEPKG_CMD
     MAKEPKG_CMD = module.get_bin_path('makepkg', True)
 
-    if not aur_package_exists(module, name):
+    package = find_aur_package(module, name)
+
+    if not package:
+        print('failed')
         module.fail_json(changed=False, msg='Couldn\'t find package ' + name)
 
     if state == 'present' or state == 'latest':
-        aur_package_fetch(module, name)
-        installed = aur_package_install(module, name)
+        fetch_aur_package(module, {'name': name, 'url_path'=package['URLPath']})
+        installed = install_aur_package(module, name)
         module.exit_json(changed=True)
     elif state == 'absent':
-        aur_package_remove(name)
+        remove_aur_package(name)
         module.exit_json(changed=True)
 
 if __name__ == '__main__':
